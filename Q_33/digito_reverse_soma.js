3508a5e6030399fc0fd3074412aacee42612bab8
Diego <102397299+Daliego@users.noreply.github.com>
2022-04-03 20:54:02 -0300

    Add files via upload

---
+++ Q_33/digito_reverse_soma.js
@@ -0,0 +1,16 @@
+import{print, input} from './io_utils.js' 
+
+function main(){
+    //entrada
+    const numero = Number(input('Numero 3 dígitos:'))
+    
+    const numero2 = Number(numero.toString().split('').reverse().join(''))
+    
+    
+
+    //processamento
+    const diferença = numero + numero2
+    //processamento
+    print(`O resultado é ${diferença}`)
+}
+main()
